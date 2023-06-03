749c0a9bcb3114958a36fef2fdeb66643a1d6f2f
Tim Eggers <eggerstimothy@gmail.com>
2023-06-03 12:23:42 -0600

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        // start button animation
+        sender.backgroundColor?.withAlphaComponent(0.7)
+        sender.alpha = 0.7
+        // play note
+        playSound(soundName: sender.currentTitle!)
+        Thread.sleep(forTimeInterval: 0.2)
+        // finish button animation
+        sender.backgroundColor?.withAlphaComponent(1.0)
+        sender.alpha = 1.0
+    }
     
-
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            // required for iOS 11.
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+        } catch let error {
+            print(error)
+        }
+    }
 }
 
